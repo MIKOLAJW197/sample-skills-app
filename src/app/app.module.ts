77ca879f32adb9d45f20f6deaa921fe1bc5029a1
Mikolaj Wolicki <wolicki.mikolaj@gmail.com>
2023-10-15 11:07:59 +0200

    feat: Init material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MaterialModule } from './material/material.module';
 
 const COMPONENTS = [NavbarComponent];
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [CommonModule],
-  exports: [...COMPONENTS],
+  imports: [CommonModule, MaterialModule],
+  exports: [...COMPONENTS, MaterialModule],
 })
 export class SharedModule {}
